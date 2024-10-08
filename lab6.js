dc6a334f9783cca78e794b3ecd659e913f3e3946
FahadAhmadSecE <n01728716@humber.ca>
2024-10-08 08:47:46 -0400

    Lab 6

---
+++ lab6.js
@@ -0,0 +1,31 @@
+//Task 1
+console.log("Task 1");
+let myNumber = 10;
+let myString = "Hello, Javascript";
+console.log(myNumber);
+console.log(myString);
+console.log("");
+//Task 2
+console.log("Task 2");
+for (let x = 0; x <= 10; x++) {
+console.log(x);
+}
+
+for (let y = 1; y <= 5; y++) {
+    console.log(3 * y);
+}
+console.log("");
+
+//Task 3
+console.log("Task 3");
+function addNumber(num1,num2) {
+return num1 + num2
+}
+
+console.log(addNumber(10, 5));
+
+function multiplyNumbers(a, b) {
+    return a * b
+}
+
+console.log(multiplyNumbers(3, 2));
